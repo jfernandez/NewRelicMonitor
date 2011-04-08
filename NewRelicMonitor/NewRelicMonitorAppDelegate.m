9237e4ea4b9caeb6132a719a5ce4af8c9a337739
Jose Fernandez <josefernandez@me.com>
2011-04-08 01:26:28 -0500

    XML

--- NewRelicMonitor/NewRelicMonitorAppDelegate.m
+++ NewRelicMonitor/NewRelicMonitorAppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "NewRelicMonitorAppDelegate.h"
+#import "ASIHTTPRequest.h"
+#import "GDataXMLNode.h"
 
 @implementation NewRelicMonitorAppDelegate
 
@@ -14,7 +16,35 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    // https://rpm.newrelic.com/accounts/11042/applications/106245/threshold_values.xml
+    // 0c3244d7f2494f511bd4f25a42e4ddfe6e2195293886c75
+    
+    NSURL *url = [NSURL URLWithString:@"https://rpm.newrelic.com/accounts/11042/applications/106245/threshold_values.xml"];
+    ASIHTTPRequest *request = [ASIHTTPRequest requestWithURL:url];
+    [request addRequestHeader:@"x-api-key" value:@"0c3244d7f2494f511bd4f25a42e4ddfe6e2195293886c75"];
+    [request startSynchronous];
+    NSError *error = [request error];
+    if (!error) {
+        NSString *response = [request responseString];
+        NSError *error;
+        GDataXMLDocument *doc = [GDataXMLDocument alloc];
+        [doc initWithXMLString:response options:0 error:&error];
+        
+        NSLog(@"%@", doc.rootElement);
+    
+        
+        [doc release];
+        //NSLog(@"%@", response);
+    }
+}
+
+-(void)awakeFromNib{
+    NSStatusBar *bar = [NSStatusBar systemStatusBar];
+    statusItem = [bar statusItemWithLength:NSVariableStatusItemLength];
+    [statusItem retain];
+    [statusItem setMenu:statusMenu];
+    [statusItem setTitle:@"Status"];
+    [statusItem setHighlightMode:YES];
 }
 
 @end
